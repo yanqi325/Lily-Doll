f87888ab48f02c52a812df2ac1e13bbc966e9b11
yanqi325 <yanqi0325@gmail.com>
2024-03-17 00:59:44 +0800

    Change profile user name (retrieve from firebase)

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -20,59 +20,88 @@ class _ProfilePageScreenState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
+    AuthHelper authHelper = new AuthHelper();
+
+
     return Scaffold(
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(85), // Set the preferred height
           child: appBar(title: 'Profile',
           icon: null,),
         ),
-        body: Container(
-          color: backgroundColor,
-          child: Padding(
-            padding: const EdgeInsets.only(top:3,left: 18,right: 18),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Avatar(radius: 36,align: Alignment.center,),
-                Text(Users.username,
-                  textAlign: TextAlign.center,
-                  style: appBarLabel.copyWith(fontSize: 18, color: Colors.black),
-                ),
-                SizedBox(height: 12,),
-                Cardboard(label: 'Name', labelContent: Users.username, icon: Icons.people_alt,),
-                SizedBox(height: 8,),
-                Cardboard(label: 'Email', labelContent: Users.email, icon: Icons.email,),
-                SizedBox(height: 8,),
-                Cardboard(label: 'Password', labelContent: 'Tap to Change Password', icon: Icons.lock,),
-                SizedBox(height: 8,),
-                Cardboard(label: 'Phone Number', labelContent: Users.phoneNum, icon: Icons.phone_android,),
-                SizedBox(height: 8,),
-                Cardboard(label: 'Payment', labelContent: 'Tap to Change Payment', icon: Icons.credit_card,),
-                SizedBox(height: 8,),
-                Cardboard(label: 'Privacy Policy', labelContent: 'Tap to See Privacy Policy', icon: Icons.privacy_tip,),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: InkWell(
-                      onTap: (){
-                        print('Log out');
-                        AuthHelper authHelper = new AuthHelper();
-                        authHelper.signOutUser();
-                        Navigator.pushNamed(context, WelcomeScreen.id);
-                      },
-                      child: Text('LOG OUT',
-                        style: signUpButtonTextStyle.copyWith(color: purple4, fontSize: 16, fontWeight: FontWeight.normal),
+        body: FutureBuilder<String?>(
+        future: authHelper.getCurrentUsername(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              String? userName = snapshot.data;
+              return Container(
+                color: backgroundColor,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 3, left: 18, right: 18),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Avatar(radius: 36, align: Alignment.center,),
+                      Text(userName!,
+                        textAlign: TextAlign.center,
+                        style: appBarLabel.copyWith(
+                            fontSize: 18, color: Colors.black),
                       ),
-                    ),
+                      SizedBox(height: 12,),
+                      Cardboard(label: 'Name',
+                        labelContent: userName,
+                        icon: Icons.people_alt,),
+                      SizedBox(height: 8,),
+                      Cardboard(label: 'Email',
+                        labelContent: Users.email,
+                        icon: Icons.email,),
+                      SizedBox(height: 8,),
+                      Cardboard(label: 'Password',
+                        labelContent: 'Tap to Change Password',
+                        icon: Icons.lock,),
+                      SizedBox(height: 8,),
+                      Cardboard(label: 'Phone Number',
+                        labelContent: Users.phoneNum,
+                        icon: Icons.phone_android,),
+                      SizedBox(height: 8,),
+                      Cardboard(label: 'Payment',
+                        labelContent: 'Tap to Change Payment',
+                        icon: Icons.credit_card,),
+                      SizedBox(height: 8,),
+                      Cardboard(label: 'Privacy Policy',
+                        labelContent: 'Tap to See Privacy Policy',
+                        icon: Icons.privacy_tip,),
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                          child: InkWell(
+                            onTap: () {
+                              print('Log out');
+                              AuthHelper authHelper = new AuthHelper();
+                              authHelper.signOutUser();
+                              Navigator.pushNamed(context, WelcomeScreen.id);
+                            },
+                            child: Text('LOG OUT',
+                              style: signUpButtonTextStyle.copyWith(
+                                  color: purple4,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ),
+                        ),
+                      ),
+
+
+                    ],
                   ),
                 ),
-
-
-
-              ],
-            ),
-          ),
-        ),
+              );
+            }
+          }
+          return Container();
+        },
+        )
 
 
     );
